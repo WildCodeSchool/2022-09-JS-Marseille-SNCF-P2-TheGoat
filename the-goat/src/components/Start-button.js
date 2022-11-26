aa6a781eff12db62aca0d37d2390ff69fda575c9
MORTREUX Christophe <7610671u@commun.ad.sncf.fr>
2022-11-26 11:01:29 +0100

    Create files Start-button js and css

---
+++ the-goat/src/components/Start-button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Start-button.css';
+
+const StartGame = () => {
+    return (
+        <div className='start-button'>
+           <img className='ballon' src='the-goat/src/images/ballon-bb.jpg' ></img>
+            
+        </div>
+    );
+};
+
+export default Start-button;
