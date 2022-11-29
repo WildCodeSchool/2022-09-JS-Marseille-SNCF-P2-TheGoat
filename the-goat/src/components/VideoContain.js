ce84e0820f612f311747760c5938551df6ddb1f5
MORTREUX Christophe <7610671u@commun.ad.sncf.fr>
2022-11-29 17:52:26 +0100

    add  Video

--- the-goat/src/components/VideoContain.js
+++ the-goat/src/components/VideoContain.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 const VideoContain = () => {
-    return (
+    const handleClick = event => {
+        // 👇️ refers to the video element
+        console.log(event.target);
+    
+        console.log('video clicked');
+      };
+    
+      return (
         <div>
-            
+          <img
+            src="https://the-goat/src/assets/video/espn_nba_2022_-_rebrand.mp4"
+            alt="Movie Nba"
+            onClick={handleClick}
+          />
         </div>
-    );
-};
+      );
+    };
+    
+
 
 export default VideoContain;
