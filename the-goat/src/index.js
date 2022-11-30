de0e2ea229cf85d9e5cb0b29ad6c2e02cbe94605
ESNAY Roland (SNCF VOYAGEURS / Direction de Lignes TER Est Provence Alpes / SIEGE UO RC TOULON) <7807212a@commun.ad.sncf.fr>
2022-11-30 11:56:19 +0100

    add menuBurger and Navbar

--- the-goat/src/index.js
+++ the-goat/src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Stats from "./routes/Stats";
+import Home from "./routes/Home";
+import Game from "./routes/Game";
+import Season from "./routes/Season";
+import Team from "./routes/Team";
+import Fight from "./routes/Fight";
+import Features from "./routes/Features";
+import Navbar from "./components/Navbar";
+import "./components/Navbar.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const AppLayout = () =>(
+  
+  <>
+  <Navbar/>
+  <Outlet/>
+  </>
+)
+const router = createBrowserRouter([
+  {
+    element:<AppLayout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "stats",
+        element: <Stats/>,
+      },
+      {
+        path: "game",
+        element: <Game/>,
+      },
+      {
+        path: "season",
+        element: <Season/>,
+      },
+      {
+        path: "team",
+        element: <Team/>,
+      },
+      {
+        path: "fight",
+        element: <Fight/>,
+      },
+      {
+        path: "features",
+        element: <Features/>,
+      },
+    ]
+  }
+  
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
