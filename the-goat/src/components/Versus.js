3d71c15a12d3a56236e9131587fd1e74e013e0f8
Eric Rouviere <eric.rouviere2@sncf.fr>
2022-12-14 11:58:37 +0100

    commit statik without pictures

--- the-goat/src/components/Versus.js
+++ the-goat/src/components/Versus.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import cardImageSrc from "./images/Kevin-Durant-VS.png";
+import Steph from './images/Curry-VS.jpg';
 import './Versus.css';
 
 
@@ -18,15 +19,21 @@ function Versus() {
           </header>
           <div className='Body-versus'>
             <div className='Versus-legend-container'>
-                <div className='Versus-legend'>
-                    <div className='Image-Versus-legend'><img src={cardImageSrc} className="playerImg"
-                     alt="image des joueurs"/></div>                        
-                    
+                <div className='Versus-legend-stats'>
+                  <h3 className='Stats-h3'>Stats Legend</h3>
                 </div>
-                <div className='Versus-legend1'>
+                <button className='Vote-versus-legend'>Vote</button>
+                <div className='Versus-legend'>                   
+                    <div className='Image-Versus-legend'><img src={Steph} className="playerImg"
+                     alt="image des joueurs"/></div>                                             
+                </div>
+                <div className='Versus-legend1'>                    
                     <div className='Image-Versus-legend1'><img src={cardImageSrc} className="playerImg"
-                        alt="image des joueurs"/></div>                        
-                    
+                        alt="image des joueurs"/></div>                                               
+                </div>
+                <button className='Vote-versus-legend1'>Vote</button>
+                <div className='Versus-legend1-stats'>
+                  <h3 className='Stats-h3'>Stats Legend</h3>
                 </div>
             </div>            
 
