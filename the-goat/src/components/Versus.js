469f8fb63aa20d66c1db62faa68723f00a90eb78
Eric Rouviere <eric.rouviere2@sncf.fr>
2022-12-14 15:12:48 +0100

    commit versus x4 without good pictures names

--- the-goat/src/components/Versus.js
+++ the-goat/src/components/Versus.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import cardImageSrc from "./images/Kevin-Durant-VS.png";
-import Steph from './images/Curry-VS.jpg';
+import Kevin from "./images/durant.png";
+import Steph from './images/curry-vignette.png';
 import './Versus.css';
 
 
@@ -21,19 +21,51 @@ function Versus() {
             <div className='Versus-legend-container'>
                 <div className='Versus-legend-stats'>
                   <h3 className='Stats-h3'>Stats Legend</h3>
+                  <br/>
+                  <p>Points: 30</p>
+                  <br/>
+                  <p>3 Points: 15</p>
+                  <br/>
+                  <p>2 Points: 18</p>
+                  <br/>
+                  <p>Free Throw % : 85.9</p>
+                  <br/>
+                  <p>Wins : 14</p>
+                  <br/>
+                  <p>Loss : 12</p>
+                  <br/>
+                  <p>Championship : 4</p>
                 </div>
                 <button className='Vote-versus-legend'>Vote</button>
-                <div className='Versus-legend'>                   
+                
+                <div className='Versus-legend'>
+                    <div className='Versus-legend-name'>Stephen Curry</div>                  
                     <div className='Image-Versus-legend'><img src={Steph} className="playerImg"
                      alt="image des joueurs"/></div>                                             
                 </div>
-                <div className='Versus-legend1'>                    
-                    <div className='Image-Versus-legend1'><img src={cardImageSrc} className="playerImg"
+                <div className='Versus-legend1'>
+                    <div className='Versus-legend1-name'>Kevin Durant</div>                    
+                    <div className='Image-Versus-legend1'><img src={Kevin} className="playerImg"
                         alt="image des joueurs"/></div>                                               
                 </div>
+                
                 <button className='Vote-versus-legend1'>Vote</button>
                 <div className='Versus-legend1-stats'>
                   <h3 className='Stats-h3'>Stats Legend</h3>
+                  <br/>
+                  <p>Points: 30</p>
+                  <br/>
+                  <p>3 Points: 15</p>
+                  <br/>
+                  <p>2 Points: 18</p>
+                  <br/>
+                  <p>Free Throw % : 85.9</p>
+                  <br/>
+                  <p>Wins : 14</p>
+                  <br/>
+                  <p>Loss : 12</p>
+                  <br/>
+                  <p>Championship : 4</p>
                 </div>
             </div>            
 
