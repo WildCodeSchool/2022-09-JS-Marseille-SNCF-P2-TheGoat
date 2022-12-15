781ff5287b502d02e83781a096cc6734215325f7
Eric Rouviere <eric.rouviere2@sncf.fr>
2022-12-15 09:22:46 +0100

    commit games bug fixed

--- the-goat/src/components/CardLegendContainer.js
+++ the-goat/src/components/CardLegendContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import CardLegendContain from './CardLegendContain';
 import './CardLegendContain';
+import './CardLegendContainer.css';
 
 function CardLegendContainer(){
     return(
