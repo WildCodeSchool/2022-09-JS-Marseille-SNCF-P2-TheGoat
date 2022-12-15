4250def28bda1eecac0affcd032de1ac51aee4fa
MORTREUX Christophe <7610671u@commun.ad.sncf.fr>
2022-12-15 23:22:33 +0100

    create 6 division cards

--- the-goat/src/components/CardContain.js
+++ the-goat/src/components/CardContain.js
@@ -5,11 +5,12 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 
-import WorkIcon from '@mui/icons-material/Work';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+// import WorkIcon from '@mui/icons-material/Work';
+// import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import Audiowide from '../fonts/Audiowide-Regular.ttf'
 import './CardContain.css';
 import "../components/Teams.css";
+
 
 export default function CardContain({ dataStanding }) {
   console.log(dataStanding, "DATASTANDING");
@@ -24,56 +25,40 @@ export default function CardContain({ dataStanding }) {
   
   return (
 
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', fontFamily:{Audiowide} }}>
+    <List sx={{ width: '100%', maxWidth: 400, bgcolor: 'background.paper', fontFamily:{Audiowide} }}>
       
       {dataStanding?.standings_rows?.map((dataStandingMap) => {
               console.log(dataStandingMap, "INSIDE MAP");
 
         return (
+       <>
+       <div>
         <ListItem className='list-item'>
           <ListItemAvatar className='list-avatar'>
-            <Avatar className='avatar'>
+            <Avatar className='avatar' 
+              sx={{ width: 50, height: 50, marginRight:'1em', marginLeft:'2em'}}
+            >
               <img
               className='image-icon' 
               src={dataStandingMap?.team?.logo}
-              alt="logo Team"/>
+              alt="logo Team"
+              sx={{ width: 62, height: 62, }}
+              slotProps={{ thumb: { className: 'thumb' } }}
+              />
             </Avatar>
           </ListItemAvatar>
-          <ListItemText  primary={dataStandingMap?.team?.name_full} />
+          <ListItemText
+            primary={dataStandingMap?.team?.name_full}
+            sx={{marginLeft:"1em"}}
+            
+             />
+        
+        
         </ListItem>
-        /*<ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <WorkIcon className='work-icon'/>
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Cleveland Cavaliers" />
-        </ListItem>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <BeachAccessIcon className='beach-icon' />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Indiana Pacers" />
-        </ListItem>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <ImageIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Chicago Bulls" />
-        </ListItem>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <WorkIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Detroit Pistons" />
-        </ListItem> */
-      
+        </div>
+        
+        </>
+
         
 
         );
--- the-goat/src/routes/Team.js
+++ the-goat/src/routes/Team.js
@@ -15,7 +15,7 @@ function Team (){
         method: "GET",
         url: "https://sportscore1.p.rapidapi.com/seasons/21630/standings-tables",
         headers: {
-          "X-RapidAPI-Key": "30eac0cc41mshc3c64b153401231p173519jsna70ad75703ae",
+          "X-RapidAPI-Key": "92af1b4e9emsh6034f2ef537bdbdp120636jsnd456f52028d0",
           "X-RapidAPI-Host": "sportscore1.p.rapidapi.com",
         },
       };
@@ -32,31 +32,135 @@ function Team (){
       fetchStanding();
     }, []);
     console.log(dataStanding, "STATE");
-    
-//     const [dataStanding2, setDataStanding2] = useState();
+//-----------------------------------------------------------------    
+    const [dataStanding2, setDataStanding2] = useState();
   
-//     const fetchStanding2 = () => {
-//       const options = {
-//         method: "GET",
-//         url: "https://sportscore1.p.rapidapi.com/seasons/21630/standings-tables",
-//         headers: {
-//           "X-RapidAPI-Key": "30eac0cc41mshc3c64b153401231p173519jsna70ad75703ae",
-//           "X-RapidAPI-Host": "sportscore1.p.rapidapi.com",
-//         },
-//       };
-//       axios
-//         .request(options)
-//         .then((response) => response.data)
-//         .then((data) => {
-//           setDataStanding2(data.data[2]);
-//           console.log(data);
-//         });
+    const fetchStanding2 = () => {
+      const options = {
+        method: "GET",
+        url: "https://sportscore1.p.rapidapi.com/seasons/21630/standings-tables",
+        headers: {
+          "X-RapidAPI-Key": "92af1b4e9emsh6034f2ef537bdbdp120636jsnd456f52028d0",
+          "X-RapidAPI-Host": "sportscore1.p.rapidapi.com",
+        },
+      };
+      axios
+        .request(options)
+        .then((response) => response.data)
+        .then((data) => {
+          setDataStanding2(data.data[4]);
+          console.log(data);
+        });
         
-//     };
-//     useEffect(() => {
-//       fetchStanding2();
-//     }, []);
-//     console.log(dataStanding2, "STATE-2");
+    };
+    useEffect(() => {
+      fetchStanding2();
+    }, []);
+    console.log(dataStanding2, "STATE-2");
+//------------------------------------------------------------
+
+const [dataStanding3, setDataStanding3] = useState();
+  
+    const fetchStanding3 = () => {
+      const options = {
+        method: "GET",
+        url: "https://sportscore1.p.rapidapi.com/seasons/21630/standings-tables",
+        headers: {
+          "X-RapidAPI-Key": "92af1b4e9emsh6034f2ef537bdbdp120636jsnd456f52028d0",
+          "X-RapidAPI-Host": "sportscore1.p.rapidapi.com",
+        },
+      };
+      axios
+        .request(options)
+        .then((response) => response.data)
+        .then((data) => {
+          setDataStanding3(data.data[5]);
+          console.log(data);
+        });
+        
+    };
+    useEffect(() => {
+      fetchStanding3();
+    }, []);
+    console.log(dataStanding3, "STATE-3");
+//------------------------------------------------------------
+
+const [dataStanding4, setDataStanding4] = useState();
+  
+    const fetchStanding4 = () => {
+      const options = {
+        method: "GET",
+        url: "https://sportscore1.p.rapidapi.com/seasons/21630/standings-tables",
+        headers: {
+          "X-RapidAPI-Key": "92af1b4e9emsh6034f2ef537bdbdp120636jsnd456f52028d0",
+          "X-RapidAPI-Host": "sportscore1.p.rapidapi.com",
+        },
+      };
+      axios
+        .request(options)
+        .then((response) => response.data)
+        .then((data) => {
+          setDataStanding4(data.data[6]);
+          console.log(data);
+        });
+        
+    };
+    useEffect(() => {
+      fetchStanding4();
+    }, []);
+    console.log(dataStanding4, "STATE-4");
+//------------------------------------------------------------
+
+const [dataStanding5, setDataStanding5] = useState();
+  
+    const fetchStanding5 = () => {
+      const options = {
+        method: "GET",
+        url: "https://sportscore1.p.rapidapi.com/seasons/21630/standings-tables",
+        headers: {
+          "X-RapidAPI-Key": "92af1b4e9emsh6034f2ef537bdbdp120636jsnd456f52028d0",
+          "X-RapidAPI-Host": "sportscore1.p.rapidapi.com",
+        },
+      };
+      axios
+        .request(options)
+        .then((response) => response.data)
+        .then((data) => {
+          setDataStanding5(data.data[7]);
+          console.log(data);
+        });
+        
+    };
+    useEffect(() => {
+      fetchStanding5();
+    }, []);
+    console.log(dataStanding5, "STATE-5");
+//------------------------------------------------------------
+
+const [dataStanding6, setDataStanding6] = useState();
+  
+    const fetchStanding6 = () => {
+      const options = {
+        method: "GET",
+        url: "https://sportscore1.p.rapidapi.com/seasons/21630/standings-tables",
+        headers: {
+          "X-RapidAPI-Key": "92af1b4e9emsh6034f2ef537bdbdp120636jsnd456f52028d0",
+          "X-RapidAPI-Host": "sportscore1.p.rapidapi.com",
+        },
+      };
+      axios
+        .request(options)
+        .then((response) => response.data)
+        .then((data) => {
+          setDataStanding6(data.data[8]);
+          console.log(data);
+        });
+        
+    };
+    useEffect(() => {
+      fetchStanding6();
+    }, []);
+    console.log(dataStanding6, "STATE-6");
 
     return (
         <div className="teams">
@@ -77,11 +181,54 @@ function Team (){
                                 <CardContain dataStanding={dataStanding} />
                             </div>
                     </div>
+                   <div className='card-teams'>
+                            <div className='title-card'>
+                            Atlantic Division
+                            </div>
+                            <div className='list-card'>
+                                <CardContain dataStanding={dataStanding2} />
+                            </div>
+                    </div>
+                
+                
+                    <div className='card-teams'>
+                            <div className='title-card'>
+                            Southeast Division
+                            </div>
+                            <div className='list-card'>
+                                <CardContain dataStanding={dataStanding3} />
+                            </div>
+                    </div>
+                </div>
+                <div className='card-teams-container'>
+                
+                    <div className='card-teams'>
+                            <div className='title-card'>
+                            Northwest Division
+                            </div>
+                            <div className='list-card'>
+                                <CardContain dataStanding={dataStanding4} />
+                            </div>
+                    </div>
+                    <div className='card-teams'>
+                            <div className='title-card'>
+                            Pacific Division
+                            </div>
+                            <div className='list-card'>
+                                <CardContain dataStanding={dataStanding5} />
+                            </div>
+                    </div>
+                    <div className='card-teams'>
+                            <div className='title-card'>
+                            Southwest Division
+                            </div>
+                            <div className='list-card'>
+                                <CardContain dataStanding={dataStanding6} />
+                            </div>
+                    </div>
                 </div>
                 
-                {/* <div className="table-container">
-                <CustomizedTables2 dataStanding2={dataStanding2} />
-                </div> */}
+                
             </div>
             <div className='teamsMapButtons'>
                 <Map />
