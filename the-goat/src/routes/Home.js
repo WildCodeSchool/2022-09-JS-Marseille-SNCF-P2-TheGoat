7d61946801a8f95444ff6c09a9e799cf6da4893f
ESNAY Roland (SNCF VOYAGEURS / Direction de Lignes TER Est Provence Alpes / SIEGE UO RC TOULON) <7807212a@commun.ad.sncf.fr>
2022-12-16 16:12:08 +0100

    resolve bug on startball

--- the-goat/src/routes/Home.js
+++ the-goat/src/routes/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import StartButton from "../components/StartButton";
 import StartVideo from "../components/StartVideo";
 
-import "../components/Home.css";
+import "./Home.css";
 
 
 function Home (){
