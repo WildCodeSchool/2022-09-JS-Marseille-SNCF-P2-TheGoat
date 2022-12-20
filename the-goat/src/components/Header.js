baf0d1bc6bb4f38d03823336b95246d9f0900a24
ESNAY Roland (SNCF VOYAGEURS / Direction de Lignes TER Est Provence Alpes / SIEGE UO RC TOULON) <7807212a@commun.ad.sncf.fr>
2022-12-20 14:22:55 +0100

    work on responsive

--- the-goat/src/components/Header.js
+++ the-goat/src/components/Header.js
@@ -4,18 +4,19 @@ import { useLocation } from "react-router";
 
 function Header() {
   let location = useLocation();
-  
-  const splitUrl = location?.pathname.replace("/","" );
 
+  const splitUrl = location?.pathname.replace("/", "");
 
   return (
     <header className="Header">
-      <div  className="Header-main-title"><h1>{splitUrl.toUpperCase()}</h1></div>
+      <div className="Header-main-title">
+        <h1>{splitUrl.toUpperCase()}</h1>
+      </div>
       <div className="Header-title">
         <h2 className="Header-subtitle">The G.O.A.T</h2>
         <div className="Header-p">
           <p>32 Legends</p>
-          <p>4 Weeks</p>
+          <p>5 Weeks</p>
         </div>
       </div>
     </header>
